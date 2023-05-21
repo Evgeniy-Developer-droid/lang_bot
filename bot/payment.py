c8bcb765104db8987e3ad171f5212c325ce64599
Evgeniy-Developer-droid <evgeniy.python.developer@gmail.com>
2023-05-21 18:42:36 +0300

    first commit

--- bot/payment.py
+++ bot/payment.py
@@ -1,19 +1,26 @@
 from liqpay.liqpay3 import LiqPay
 import os
+import sys
+
+sys.stdout.flush()
 
 def pay_link_generate(amount, description, token, order_id):
-    liqpay = LiqPay(os.environ.get("LIQPAY_PUBLIC", ""), os.environ.get("LIQPAY_SECRET", ""))
-    params = {
-        'action': 'pay',
-        'currency': 'UAH',
-        'order_id': token,
-        'version': '3',
-        'amount': amount,
-        'description': description,
-        'sandbox': int(os.environ.get("LIQPAY_SANDBOX", 0)), # sandbox mode, set to 1 to enable it
-        'server_url':  f'{os.environ.get("SERVER_URL", "http://localhost:8000")}/manager/liqpay-callback'
-    }
-    signature = liqpay.cnb_signature(params)
-    data = liqpay.cnb_data(params)
-    link = f"https://www.liqpay.ua/api/3/checkout?data={data}&signature={signature}"
-    return link
+    try:
+        liqpay = LiqPay(os.environ.get("LIQPAY_PUBLIC", ""), os.environ.get("LIQPAY_SECRET", ""))
+        params = {
+            'action': 'pay',
+            'currency': 'UAH',
+            'order_id': token,
+            'version': '3',
+            'amount': amount,
+            'description': description,
+            'sandbox': 0, # sandbox mode, set to 1 to enable it
+            'server_url':  f'{os.environ.get("SERVER_URL", "http://localhost:8000")}/manager/liqpay-callback'
+        }
+        signature = liqpay.cnb_signature(params)
+        data = liqpay.cnb_data(params)
+        link = f"https://www.liqpay.ua/api/3/checkout?data={data}&signature={signature}"
+        return link
+    except Exception as e:
+        print("liqpay ", e, flush=True)
+        return ""
